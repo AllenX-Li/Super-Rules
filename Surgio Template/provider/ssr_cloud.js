5be27af41a3f351c98b524a970ebe03702ceb9c6
Allen Li <ljl178824583@outlook.com>
2020-07-08 09:39:20 +0800

    Surgio Template

---
+++ Surgio Template/provider/ssr_cloud.js
@@ -0,0 +1,15 @@
+'use strict';
+const { utils } = require('surgio');
+
+module.exports = {
+  url: 'https://suo.yt/5bQ0yhE',
+  type: 'shadowsocks_subscribe',
+  addFlag: true,
+  udpRelay: true,
+  tfo: true,
+
+  customFilters: {
+    providerFilter: utils.discardProviders(['回国线路 无法翻墙', '无试用节点，需要购买套餐后订阅节点', '用户']),
+    HBOFilter: utils.mergeFilters([utils.usFilter, utils.netflixFilter], true),
+  },
+};
